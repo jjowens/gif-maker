1c86e456f83e33293135dd57fb0fc95298dfc213
= <=>
2026-06-15 21:46:26 +0100

    added commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{Parser, Subcommand};
+use crate::Commands::MakeGif;
+
+#[derive(Parser)]
+#[command(name = "appname", author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    MakeGif {
+        #[clap(long)]
+        openFileDirectory: String,
+        #[clap(long, default_value = "test-output")]
+        saveGifDir: String,
+        #[clap(long, default_value = "test.gif")]
+        saveGifFilename: String
+    }
+}
+
 fn main() {
     println!("Gif-Maker");
+
+    let args = Args::parse();
+
+    match args.command {
+        Some(Commands::MakeGif { openFileDirectory, saveGifDir, saveGifFilename }) => {
+            println!("Making GIF");
+        },
+        None => (),
+    }
 }
