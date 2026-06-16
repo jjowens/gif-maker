09cc0c5b390f0e7233ca1394b613b9351f9c7fde
= <=>
2026-06-16 18:25:05 +0100

    added services

---
+++ src/services/makegif_service.rs
@@ -0,0 +1,7 @@
+pub fn make_gif(open_file_directory: &str, save_file_path: &str) -> Result<(), String> {
+    println!("Making GIF");
+    println!("- {}", open_file_directory);
+    println!("- {}", save_file_path);
+
+    Ok(())
+}
